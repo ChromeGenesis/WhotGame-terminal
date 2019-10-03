3cffdaa4f2c8519a1946dad1ba4687e1d95bca91
Genesis Chrome <49604988+Genesis123456@users.noreply.github.com>
2019-10-03 19:31:54 +0100

    Add files via upload

---
+++ manual.py
@@ -0,0 +1,47 @@
+manual="""
+====
+Whot
+====
+A Game of cards usually involving two players by default
+but it can be played by more than one (But is Not implemented)
+
+===========
+How to Play
+===========
+When/After you get your cards...
+The first player plays (you or the computer)
+the card played must always correspond with the last card present in the played cards
+This means that if the last played card was something like 'Triangle' with the
+number 5, the next player has to play a card having either 'Triangle' or having
+the number 5, else it'll be regarded as an invalid move
+If the player to play does not possess a valid card, he/she must go to the market
+
+* Note: Once a player goes to market, he/she has to wait for the opponent to play or
+go to market before he/she can play again
+
+If/When a Player has only one card left, it's declared as his last card
+If/When a Player plays his last card, Its Checkmate (The player is the winner!)...
+========
+Powerups
+========
+Powerups include 'whot' with the number 20 (by default), 
+and any card having the number '2', '14', '1', '8'
+
+=============
+Powerup Usage
+=============
+* when one Player plays the card 'whot' (having the number 20 by default) , 
+he/she can ask for any card from the opponent
+
+* if he/she plays any card having the number '2',
+ the opponent has to compulsorily pick two cards from the market
+
+* if he/she plays any card having the number '1',
+ the opponent has to hold on while he/she plays another card
+
+* if he/she plays any card haivng the number '14',
+ the opponent has to go to market once
+
+* if he/she plays any card having the number '8',
+ the opponent also has to hold on...
+"""
